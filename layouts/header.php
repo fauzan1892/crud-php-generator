955ad6e71a9aec7e1112fa67e575fada1e36fe21
Fauzan Falah <fauzan1892@codekop.com>
2021-05-30 15:46:17 +0700

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -29,8 +29,8 @@
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="<?= $baseURL;?>assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
     
-    <!--   Core JS Files   -->
-    <script src="<?= $baseURL;?>assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <!--   Core JS Files   -->   
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 	<script src="<?= $baseURL;?>assets/js/bootstrap.min.js" type="text/javascript"></script>
     <!--  Notifications Plugin    -->
     <script src="<?= $baseURL;?>assets/js/bootstrap-notify.js"></script>
@@ -42,9 +42,22 @@
 
     <script type="text/javascript" src="<?= $baseURL;?>assets/datatables/jquery.dataTables.js"></script>
     <script type="text/javascript" src="<?= $baseURL;?>assets/datatables/dataTables.bootstrap.js"></script>
-	<script type="text/javascript">
+	<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+    <script type="text/javascript">
 		//datatable
-		$(function () {	$("#example1").DataTable(); });
+		$(function () {	
+            $("#example1").DataTable({
+                dom: 'Bfrtip',
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+            }); 
+        });
 	</script>
 </head>
 <body>
